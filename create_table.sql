62b90faeeca395cb0b4da601086fbf588ed34a17
aminata <caminatamagath@gamil.com>
2024-01-12 15:09:52 +0100

    add import

--- create_table.sql
+++ create_table.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS customers  (
     customer_zip_prefix INTEGER,
     customer_city TEXT,
     customer_state TEXT
-
+    
     );
     --create geolocation table
     CREATE TABLE IF NOT EXISTS geolocation (
@@ -23,52 +23,53 @@ CREATE TABLE IF NOT EXISTS customers  (
     geolocation_state TEXT
 
 
-    
-    );
+);
     --create orders_items_dataset table
     CREATE TABLE IF NOT EXISTS order_item (
     order_item_id INTEGER PRIMARY KEY,
-    order_id TEXT FOREIGN KEY 1,
-    product_id TEXT FOREIGN KEY 2,
-    seller_id TEXT FOREIGN KEY 3,
+    order_id TEXT ,
+    product_id TEXT ,
+    seller_id TEXT ,
     shipping_limit_date TEXT,
     price FLOAT,
-    freight_value FLOAT
-    FOREIGN KEY(order_item_id, product_id, seller_id) REFERENCES Emplacement(order_item_id, product_id, seller_id)
+    freight_value FLOAT,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id),
+    FOREIGN KEY (product_id) REFERENCES products (product_id),
+    FOREIGN KEY (seller_id) REFERENCES sellers (seller_id)
 );
 --create  order_payments_dataset table
 CREATE TABLE IF NOT EXISTS order_payment (
-     INTEGER PRIMARY KEY,
-    order_id TEXT FOREIGN KEY,
+    order_payment_id INTEGER PRIMARY KEY,
+    order_id TEXT,
     payment_sequential INTEGER,
     payment_type TEXT,
     payment_installments INTEGER,
-    payment_value FLOAT
-    FOREIGN KEY(order_id) REFERENCES Emplacement(order_id)
+    payment_value FLOAT,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
 --create order_review_dataset_clean table
 CREATE TABLE IF NOT EXISTS review (
     review_id TEXT PRIMARY KEY,
-    order_id TEXT FOREIGN KEY,
+    order_id TEXT,
     review_score INTEGER,
     review_comment_title TEXT,
     review_comment_message TEXT,
     review_creation_date TEXT,
     review_answer_timestamp TEXT,
-    timestamp_field_7 TEXT
-    FOREIGN KEY(order_id) REFERENCES Emplacement(order_id)
+    timestamp_field_7 TEXT,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id)
 );
 --create orders_dataset table
 CREATE TABLE IF NOT EXISTS orders (
     order_id TEXT PRIMARY KEY,
-    customer_id TEXT FOREIGN KEY,
+    customer_id TEXT,
     order_status TEXT,
     order_purchase_timestamp TEXT,
     order_approved_at TEXT,
     order_delivered_carrier_date TEXT,
     order_delivered_customer_date TEXT,
-    order_estimated_delivery_date TEXT
-    
+    order_estimated_delivery_date TEXT,
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
 );
 --create product_category_name_translation table
 CREATE TABLE IF NOT EXISTS product_category_name_translation (
@@ -90,7 +91,7 @@ CREATE TABLE IF NOT EXISTS product (
     
 );
 --create sellers_dataset table
-CREATE TABLE IF NOT EXISTS seller (
+CREATE TABLE IF NOT EXISTS sellers (
     seller_id TEXT PRIMARY KEY,
     seller_zip_code_prefix INTEGER,
     seller_city TEXT,
